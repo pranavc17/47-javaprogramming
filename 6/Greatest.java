c587d587b9b7a4bcb62e75a44574b87996a61f2d
pranavc <pranavc171999@github.com>
2021-05-27 23:38:29 +0530

    java

---
+++ 6/Greatest.java
@@ -0,0 +1,26 @@
+
+
+public class Greatest {
+	public static void main(String[] args) {
+		
+		
+		int x=10;
+		int y=20;
+		int z=35;
+		System.out.println("Numbers: "+x+" "+y+" "+z);
+		if(x>y && x>z)
+		{
+			System.out.println(x+" is larger");
+		
+		}
+		else if(y>z)
+		{
+		System.out.println(y+" is larger");
+		}
+		else
+		{
+			System.out.println(z+" is larger");
+		}
+
+	}
+}
---
+++ 8/Average.java
@@ -0,0 +1,20 @@
+
+
+public class Average {
+	public static void main(String[] args) {
+		
+		System.out.println("Sum and Average of 5 numbers:");
+		int num1=18;
+		int num2=52;
+		int num3=66;
+		int num4=72;
+		int num5=32;
+		
+		float sum=num1+num2+num3+num4+num5;
+		float avg=sum/5;
+		
+		System.out.println("Sum="+sum);
+		System.out.println("Average="+avg);
+	}
+
+}
